50a2f33ffc50496c50fdfa2d0c62f94d3212f9f3
Wahab Khan <167424483+wahabkhaan@users.noreply.github.com>
2024-08-01 02:41:59 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var sound = new Howl({
+    src: ['https://dl.dropbox.com/scl/fi/s26ooie6jisj0sirih0ds/RisaTos.mp3?rlkey=v4zjdk6ioyzmlag9xxem1pu1j&dl=1'],
+    autoplay: true,
+    loop: true,
+    volume: 1
+  });
